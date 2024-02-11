48f3b6efddfde690d011ec8ff59167441fe677df
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-11 17:48:59 +0100

    update email front

--- NewsLetter/resources/views/mail/subscribed.blade.php
+++ NewsLetter/resources/views/mail/subscribed.blade.php
@@ -1,1 +1,28 @@
-<h1> User has subscribed !!</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subscription Confirmation</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+<div class="container mx-auto px-4 py-8">
+    <div class="max-w-lg mx-auto relative rounded-lg overflow-hidden">
+        <img class="w-full" src="https://img.freepik.com/free-vector/monstera-leaf-biege-background-design-resource_53876-114386.jpg?w=740&t=st=1707664078~exp=1707664678~hmac=fa59e02ac3ae8e1d38c8e05c6d4d9c6297bb8926680d769c625fdc5f620404fc" alt="Monstera Leaf Image">
+        <div class="absolute inset-0 flex items-center justify-center">
+            <div class="p-6 text-center text-white">
+                <h1 class="text-2xl font-bold mb-4">Subscription Confirmation</h1>
+                <p class="text-lg mb-4">Thank you for subscribing!</p>
+                <div class="py-8">
+                    <p class="text-gray-700 mb-16">You have successfully subscribed to our newsletter. You will now receive our latest updates and news.</p>
+                </div>
+                {{--                <div class="mt-6">--}}
+                {{--                    <a href="#" class="inline-block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Back to Homepage</a>--}}
+                {{--                </div>--}}
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
