64cadcdc9dc36230354025d61dd5196b1d53bf23
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-13 16:48:25 +0100

    la redirection des pages web

--- NewsLetter/routes/web.php
+++ NewsLetter/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\MediaController;
 use App\Http\Controllers\NewsLetterController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\Authenticate;
 use App\Models\Media;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -60,86 +61,67 @@ Route::get('/test', function () {
 
 
 //redacteur
-Route::get('/redacteur', function () {
-    return view('redacteur.dashboard');
-})->middleware('auth');
-Route::get('/create', function () {
-    return view('redacteur.createTemplate');
-});
-Route::get('/templates', function () {
-    return view('redacteur.templates');
-});
-
-//Media
-//Route::get('/upload', function () {
-//    return view('redacteur.upload');
-//});
-
-Route::get  ('/upload', [MediaController::class, 'index'])->name('upload');
-
-Route::post('/upload', [MediaController::class, 'upload'])->name('upload');
-
-
-//Route::get('/all-media', function () {
-//    return view('redacteur.media');
-//});
-Route::get('/all-media', [MediaController::class, 'show']);
-
-Route::get('/members', function () {
-    return view('redacteur.subscribers');
-});
-
-//Route::get('/add-media', function () {
-//    Media::create()
-//
-//        ->addMedia(storage_path('demo/library-dublin.jpg'))
-//        ->toMediaCollection();
-//
-//});
-
-//Route::get('/subscribers', function () {
-//    return view('redacteur.subscribers');
-//});
-Route::get('/subscribers', [NewsLetterController::class, 'showS']);
 
 
 
 
 //Admin
 
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware('auth');
+Route::group(['middleware' => ['auth', 'role:admin']], function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
 
-//Route::get('/members', function () {
-//    return view('admin.subscribers');
-//});
+    Route::get('/members', [NewsLetterController::class, 'show']);
 
-Route::get('/members', [NewsLetterController::class, 'show']);
+    Route::get('/allusers', function () {
+        return view('admin.allusers');
+    });
+    Route::get('/allusers', [UserController::class, 'show']);
+    Route::post('/allusers/{id}', [UserController::class, 'update']);
+    Route::delete('/delete/{id}', [UserController::class, 'deleteUser']);
 
-Route::get('/allusers', function () {
-    return view('admin.allusers');
+    Route::get('/deletedUsers', [UserController::class, 'showDeletedUsers']);
+
+    Route::post('/restore/{id}', [UserController::class, 'restoreUser']);
+
+
+    Route::get('/templates', function () {
+        return view('admin.templates');
+    });
+
+    Route::get('/media', function () {
+        return view('admin.media');
+    });
+
 });
-Route::get('/allusers', [UserController::class, 'show']);
-Route::post('/allusers/{id}', [UserController::class, 'update']);
-Route::delete('/delete/{id}', [UserController::class, 'deleteUser']);
-
-Route::get('/deletedUsers', [UserController::class, 'showDeletedUsers']);
-
-Route::post('/restore/{id}', [UserController::class, 'restoreUser']);
 
 
-Route::get('/templates', function () {
-    return view('admin.templates');
+Route::group(['middleware' => ['auth', 'role:admin']], function () {
+    Route::get('/redacteur', function () {
+        return view('redacteur.dashboard');
+    })->middleware('auth');
+    Route::get('/create', function () {
+        return view('redacteur.createTemplate');
+    });
+    Route::get('/templates', function () {
+        return view('redacteur.templates');
+    });
+
+
+    Route::get  ('/upload', [MediaController::class, 'index'])->name('upload');
+
+    Route::post('/upload', [MediaController::class, 'upload'])->name('upload');
+
+    Route::get('/all-media', [MediaController::class, 'show']);
+
+    Route::get('/members', function () {
+        return view('redacteur.subscribers');
+    });
+
+    Route::get('/subscribers', [NewsLetterController::class, 'showS']);
+
 });
-
-Route::get('/media', function () {
-    return view('admin.media');
-});
-
-//Route::get('/editRole', function () {
-//    return view('admin.editRole');
-//});
 
 
 // Password Reset Routes
