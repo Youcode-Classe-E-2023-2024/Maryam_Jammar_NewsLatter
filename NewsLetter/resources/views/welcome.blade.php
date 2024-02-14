ef0906abd0483ba6149db7d7559e385707d4a504
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-14 17:06:00 +0100

    rendre l'input qui envoi l'email required

--- NewsLetter/resources/views/welcome.blade.php
+++ NewsLetter/resources/views/welcome.blade.php
@@ -579,7 +579,7 @@
                             class="mx-auto mt-6 w-full max-w-sm rounded-md border bg-transparent focus-within:border-blue-400 focus-within:ring focus-within:ring-blue-300 focus-within:ring-opacity-40 dark:border-gray-700 dark:focus-within:border-blue-300">
                             <form action="{{route('subscribe')}}" method="post" class="flex flex-col md:flex-row">
                                 @csrf
-                                <input type="email" name="email" placeholder="Enter your email address"
+                                <input type="email" name="email" placeholder="Enter your email address" required
                                        class="m-1 h-10 flex-1 appearance-none border-none bg-transparent px-4 py-2 text-gray-700 placeholder-gray-400 focus:placeholder-transparent focus:outline-none focus:ring-0 dark:text-gray-200"/>
 
                                 <button type="submit"
