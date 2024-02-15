b1bda7671fd30817f5ab14d40dfbf5a9a18e224c
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-16 00:45:46 +0100

    Update media.blade.php

--- NewsLetter/resources/views/admin/media.blade.php
+++ NewsLetter/resources/views/admin/media.blade.php
@@ -5,16 +5,6 @@
         <main>
             <!-- component -->
             <div class="mx-auto w-full relative flex flex-col overflow-hidden sm:py-12">
-                <div class="flex justify-end w-26 p-6">
-                    <a href="/upload" type="submit"
-                       class="m-4 bg-gray-200 hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
-                        <svg class="w-4 h-4 mr-2 transform rotate-180" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
-                        </svg>
-                        <span>Upload more..</span>
-                    </a>
-                </div>
-
                 <div class="max-w-screen-md mx-auto">
                     <div class="grid grid-cols-3 gap-6">
                         @foreach($medias as $media)
