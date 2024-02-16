e52fd74952f7ba6f8b84339007a3b0b2c1e4cc06
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-16 09:58:14 +0100

    update editor dashboard

--- NewsLetter/resources/views/redacteur/dashboard.blade.php
+++ NewsLetter/resources/views/redacteur/dashboard.blade.php
@@ -2,70 +2,140 @@
 <div class="flex">
     @include('redacteur.side')
 
-    <div class="bg-gray-500 h-full font-sans leading-normal my-auto rounded-lg shadow-2xl tracking-normal mx-auto">
-        <div class="flex justify-center">
-            <div class="bg-white rounded-lg profile-card w-96 p-6">
-                <div class="flex justify-between items-center mb-4">
-                    <div class="flex items-center mx-auto">
-                        <span class="text-2xl ml-2 text-lg font-bold text-gray-800">Welcome Editor !</span>
-                    </div>
+    <div
+        class="px-8 h-full w-full flex flex-col">
+        <div class="bg-blue-200 shadow-lg pb-3 top-0 rounded-b-3xl">
+            <div
+                class="flex  rounded-b-3xl bg-gray-100 dark:bg-gray-700 space-y-5 flex-col items-center py-7">
+                <img class="h-28 w-28 rounded-full border-2 border-blue-700"
+                     src="{{ Auth::user()->picture }}"
+                     alt="User">
+                <a href="#"> <span
+                        class="text-h1">{{ Auth::user()->name }}</span></a>
+            </div>
+            <div
+                class="grid px-7 py-2 items-center justify-around grid-cols-3 gap-4 divide-x divide-solid ">
+                <div class="col-span-1 flex flex-col items-center ">
+                    <span class="text-2xl font-bold dark:text-gray-500">4</span>
+                    <span class="text-lg font-medium 0">My templates</span>
+                </div>
+                <div class="col-span-1 px-3 flex flex-col items-center ">
+                                                        <span class="text-2xl font-bold dark:text-gray-500">
+                                                           Free</span>
+                    <span class="text-lg font-medium">Plan</span>
+                </div>
+                <div class="col-span-1 px-3 flex flex-col items-center ">
+                                                        <span class="text-2xl font-bold dark:text-gray-500">
+                                                            546</span>
+                    <span class="text-lg font-medium">My media</span>
+                </div>
+            </div>
 
+        </div>
+
+        <div
+            class="grid rounded-2xl divide-y divide-dashed hover:divide-solid  justify-evenly bg-gray-50 dark:bg-gray-300 m-3 mt-10 grid-cols-3">
+            <div class=" col-span-1  p-3">
+                <div class="flex flex-col items-center ">
+                    <a href="">
+                        <button
+                            class="tr-300">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-14 w-14 text-gray-500" fill="none"
+                                 viewBox="0 0 24 24" stroke="currentColor"
+                                 stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                            </svg>
+                            <span class="text-lg font-medium">My Profile</span>
+                        </button>
+                    </a>
                 </div>
-                <div class="flex justify-center mb-6">
-                    <img src="{{ Auth::user()->picture }}" alt="Profile"
-                         class="rounded-full border-2 border-blue-700 p-1">
+            </div>
+            <div class="col-span-1  p-3">
+                <div class="flex flex-col items-center ">
+                    <a href="">
+                        <button
+                            class="tr-300">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-14 w-14 text-gray-500" fill="none"
+                                 viewBox="0 0 24 24" stroke="currentColor"
+                                 stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"/>
+                            </svg>
+                            <span class="text-lg font-medium">Mis dinero</span>
+                        </button>
+                    </a>
                 </div>
-                <div class="text-center mb-4">
-                    <h2 class="text-xl font-semibold text-blue-900">{{ Auth::user()->name }}</h2>
-                    <p class="text-gray-600">Lorem Ipsum Neque porro quisquam
-                    </p>
+            </div>
+            <div class="col-span-1  p-3">
+                <div class="flex flex-col items-center ">
+                    <a href="">
+                        <button
+                            class="tr-300">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-14 w-14 text-gray-500" fill="none"
+                                 viewBox="0 0 24 24" stroke="currentColor"
+                                 stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"/>
+                            </svg>
+                            <span class="text-lg font-medium">Mis referidos</span>
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div class="col-span-1  p-3">
+                <div class="flex flex-col items-center ">
+                    <a href="">
+                        <button class="tr-300">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-14 w-14 text-gray-500" fill="none"
+                                 viewBox="0 0 24 24" stroke="currentColor"
+                                 stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
+                            </svg>
+                            <span class="text-lg font-medium">Mis facturas</span>
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div class="col-span-1  p-3">
+                <div class="flex flex-col items-center ">
+                    <a href="">
+                        <button class="tr-300">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-14 w-14 text-gray-500" fill="none"
+                                 viewBox="0 0 24 24" stroke="currentColor"
+                                 stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
+                            </svg>
+                            <span class="text-lg font-medium">Ayuda</span>
+                        </button>
+                    </a>
+                </div>
+            </div>
+            <div class="col-span-1 bg-blue-50 p-3">
+                <div class="flex  flex-col items-center ">
+                    <a href="">
+                        <button class="tr-300">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-14 w-14 text-gray-500" fill="none"
+                                 viewBox="0 0 24 24" stroke="currentColor"
+                                 stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
+                            </svg>
+                            <span class="text-lg font-medium">Logout</span>
+                        </button>
+                    </a>
                 </div>
             </div>
         </div>
+
     </div>
-
-    <!-- Main content -->
-    {{--    <main class="flex-1">--}}
-    {{--        <!-- component -->--}}
-    {{--        <div class="py-12">--}}
-    {{--            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">--}}
-    {{--                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">--}}
-    {{--                    <div class="p-6 bg-white border-b border-gray-200">--}}
-    {{--                        <form method="POST" action="action.php">--}}
-    {{--                            <div class="mb-4">--}}
-    {{--                                <label class="text-xl text-gray-600">Title <span class="text-red-500">*</span></label></br>--}}
-    {{--                                <input type="text" class="border-2 border-gray-300 p-2 w-full" name="title" id="title" value="" required>--}}
-    {{--                            </div>--}}
-
-    {{--                            <div class="mb-4">--}}
-    {{--                                <label class="text-xl text-gray-600">Description</label></br>--}}
-    {{--                                <input type="text" class="border-2 border-gray-300 p-2 w-full" name="description" id="description" placeholder="(Optional)">--}}
-    {{--                            </div>--}}
-
-    {{--                            <div class="mb-8">--}}
-    {{--                                <label class="text-xl text-gray-600">Content <span class="text-red-500">*</span></label></br>--}}
-    {{--                                <textarea name="content" class="border-2 border-gray-500">--}}
-
-    {{--                            </textarea>--}}
-    {{--                            </div>--}}
-
-    {{--                            <div class="flex p-1">--}}
-    {{--                                <select class="border-2 border-gray-300 border-r p-2" name="action">--}}
-    {{--                                    <option>Save and Publish</option>--}}
-    {{--                                    <option>Save Draft</option>--}}
-    {{--                                </select>--}}
-    {{--                                <button role="submit" class="p-3 bg-blue-500 text-white hover:bg-blue-400" required>Submit</button>--}}
-    {{--                            </div>--}}
-    {{--                        </form>--}}
-    {{--                    </div>--}}
-    {{--                </div>--}}
-    {{--            </div>--}}
-    {{--        </div>--}}
-
-    {{--        <script src="https://cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>--}}
-
-    {{--        <script>--}}
-    {{--            CKEDITOR.replace( 'content' );--}}
-    {{--        </script>        </main>--}}
 
 </div>
