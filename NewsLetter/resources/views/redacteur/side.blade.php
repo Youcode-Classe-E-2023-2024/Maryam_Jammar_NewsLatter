805e71c6c5579662ed91a425c17c8fccd931bb22
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-16 22:29:59 +0100

    Update side.blade.php

--- NewsLetter/resources/views/redacteur/side.blade.php
+++ NewsLetter/resources/views/redacteur/side.blade.php
@@ -492,7 +492,7 @@
                         <!-- active & hover classes 'text-gray-700 dark:text-light' -->
                         <!-- inActive classes 'text-gray-400 dark:text-gray-400' -->
                         <a
-                            href="#"
+                            href="/editProfile"
                             role="menuitem"
                             class="block p-2 text-sm text-gray-400 transition-colors duration-200 rounded-md dark:text-gray-400 dark:hover:text-light hover:text-gray-700"
                         >
