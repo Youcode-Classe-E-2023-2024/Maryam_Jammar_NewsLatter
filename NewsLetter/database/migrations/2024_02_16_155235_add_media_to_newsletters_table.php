0263f30396e7399b335c61c78c026d596c5ac888
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-16 17:06:33 +0100

    add media column to news_letters table

---
+++ NewsLetter/database/migrations/2024_02_16_155235_add_media_to_newsletters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('news_letters', function (Blueprint $table) {
+            $table->unsignedBigInteger('media');
+
+            $table->foreign('media')->references('id')->on('media')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('newsletters', function (Blueprint $table) {
+            //
+        });
+    }
+};
