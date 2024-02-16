195af23d26540dac6b1af38a54745511c0d9d0ef
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-16 17:11:33 +0100

    Update createTemplate.blade.php

--- NewsLetter/resources/views/redacteur/createTemplate.blade.php
+++ NewsLetter/resources/views/redacteur/createTemplate.blade.php
@@ -19,7 +19,7 @@
                         <div class="mb-8">
                             <label class="text-xl text-gray-600">Content <span
                                     class="text-red-500">*</span></label></br>
-                            <textarea name="content" class="border-2 border-gray-500">
+                            <textarea name="content" class="border-2 border-gray-500" required>
 
                             </textarea>
                         </div>
