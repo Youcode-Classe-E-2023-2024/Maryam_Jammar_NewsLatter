af60fdc5a2904c8ac989ea9acb4ba4f90ee1be64
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-17 00:17:55 +0100

    Update dashboard.blade.php

--- NewsLetter/resources/views/redacteur/dashboard.blade.php
+++ NewsLetter/resources/views/redacteur/dashboard.blade.php
@@ -120,18 +120,20 @@
             </div>
             <div class="col-span-1 bg-blue-50 p-3">
                 <div class="flex  flex-col items-center ">
-                    <a href="">
-                        <button class="tr-300">
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                 class="h-14 w-14 text-gray-500" fill="none"
-                                 viewBox="0 0 24 24" stroke="currentColor"
-                                 stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
-                            </svg>
-                            <span class="text-lg font-medium">Logout</span>
-                        </button>
-                    </a>
+                    <button class="tr-300">
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             class="h-14 w-14 text-gray-500" fill="none"
+                             viewBox="0 0 24 24" stroke="currentColor"
+                             stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
+                        </svg>
+
+                            <button type="submit" class="text-lg font-medium">Logout</button>
+                        </form>
+                    </button>
                 </div>
             </div>
         </div>
