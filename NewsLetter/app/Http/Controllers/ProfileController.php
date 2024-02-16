8bdbcef07a341521d5a6a965f8786ecf0d145017
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-02-16 10:02:58 +0100

    Create ProfileController.php

---
+++ NewsLetter/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function show()
+    {
+        $user = Auth::user();
+        return view('profile.editProfile', compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users,email,'.$user->id,
+        ]);
+
+        $user->name = $validatedData['name'];
+        $user->email = $validatedData['email'];
+
+        $user->save();
+
+        return redirect()->route('profile.profile')->with('success', 'Profile updated successfully');
+    }
+}
